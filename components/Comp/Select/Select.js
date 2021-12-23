b5937a39bfbabba8db966a4ee14814be7b39092a
sivastu <siva100100100@gmail.com>
2021-12-23 20:00:47 +0530

    hhh

--- components/Comp/Select/Select.js
+++ components/Comp/Select/Select.js
@@ -1,40 +1,116 @@
 import React,{useState} from 'react'
-import {StyleSheet, Text, View, StatusBar,Dimensions,Image,TouchableOpacity,button } from 'react-native'
+import {StyleSheet, Text, View, StatusBar,Dimensions,Image,TouchableOpacity } from 'react-native'
 import {Column as Col, Row} from 'react-native-flexbox-grid';
-
 
 const wi = Dimensions.get('window').width;
 const hi = Dimensions.get('window').height
 
-
-const serve =async ()=>{
- await fetch('/port',{ 
-      headers:{
-          "Content-Type":"application/json",
-            }
-          })
-          .then(result=>{
-             console.log(result)
-          }).catch(err=>{
-              console.log(err)
-          })
-      }
-
 const Select =( props)=>{
   
+  
+  let imglink= 'https://www.w3schools.com/images/w3schools_green.jpg'
+  
+  let all ='Alljwiiwhw uwh3hue euwuwineeuejje sjeuhehejeie euen3 eie wiebe eiebe wjeneueneijwhehneieneuebuenejrikekrij4urjrkjrn4jrnnejrj4m5k4i4mrkeijek4i3keoekkroe9rkrkdikddkisjeudjdidkmejeenijddjien amayroney'
+  let dot = '.....';
+  let data = all.substring(0,50)
+  
     return(
-      <View>
-      
-          <button onClick={serve}>Recive</button>
-          <button>Connect</button>
+      <View style={styles.rowstyle}>
+      <Row  size={12}>
+        <Col style={styles.alignh} sm={4} md={6} lg={3}>
+          <Image  source={imglink} style={styles.imgs} />
+        </Col>
+        <Col style={styles.alignvi} sm={8} md={6} lg={3}>
+          <TouchableOpacity
+            onPress={() => {
+              console.log('presed')
+            }} >
+            <h6>title of the day </h6>
+            <Text style={styles.vicolor}>
+            {data}{dot}
+            </Text>
+          </TouchableOpacity>
+        </Col>
+         <Row style={styles.btnrow} size={12}>
+        <Col style={styles.catcolor} sm={6} md={6} lg={3}>
+          <TouchableOpacity style={styles.btnop}
+            onPress={() => {
+              console.log('presed')
+            }} >
+            <h6  style={{marginRight:20}}>Login</h6>
+          </TouchableOpacity>
+        </Col>
+        <Col style={styles.btnop} sm={6} md={6} lg={3}>
+          <TouchableOpacity style={styles.btnop}
+            onPress={() => {
+              console.log('presed')
+            }} >
+            <h6>Register</h6>
+          </TouchableOpacity>
+        </Col>
+      </Row>
+      </Row>
       </View>
     )
   }
 export default Select
 
 const styles = StyleSheet.create({
-   btn:{
-     width:wi-150,
-     height:hi/10
-   }
+    imgs:{
+      width: 80,
+      height: 80,
+      marginTop:'auto',
+      marginRight:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+    },
+    alignvi:{
+      paddingLeft:5,
+      marginTop:0,
+      marginRight:'auto',
+      marginBottom:'auto',
+    },
+    catcolor:{
+      marginTop:'auto',
+      marginRight:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+    },
+    vicolor:{
+      color:'#000',
+      marginTop:-5,
+      marginRight:5
+    },
+    alignh:{
+      marginTop:20,
+      marginRight:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+    },
+    rowstyle:{
+      width:wi-15,
+      height:hi-133,
+      marginTop:10,
+      marginRight:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+      borderRadius:10,
+      shadowColor: '#171717',
+      shadowOffset: {width: 0, height: 0},
+      shadowOpacity: 0.2,
+      shadowRadius: 4,
+      backgroundColor:'#fff'
+      },
+    btnrow:{
+      marginTop:20,
+      marginRight:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+    },
+    btnop:{
+      marginTop:'auto',
+      marginRight:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+    }
   })
